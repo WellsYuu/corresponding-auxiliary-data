6b5e5ddcf31f916ad5f4dcf183b7ce290f0b4b30
Wales Yu <opensource@thenx.org>
2021-11-01 11:43:30 +0800

    spring 1

---
+++ Spring源码解析及手写Spring/第1节课/第1节课/spring-anno/src/main/java/com/enjoy/cap2/config/Cap2MainConfig.java
@@ -0,0 +1,26 @@
+package com.enjoy.cap2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+
+import com.enjoy.cap1.Person;
+import com.enjoy.cap2.controller.OrderController;
+
+@Configuration
+@ComponentScan(value="com.enjoy.cap2", includeFilters={		
+		@Filter(type=FilterType.CUSTOM, classes={JamesTypeFilter.class})		
+}, useDefaultFilters=false)
+
+
+public class Cap2MainConfig {
+	//给容器中注册一个bean, 类型为返回值的类型, 
+	@Bean
+	public Person person01(){
+		return new Person("james",20);
+	}
+}
